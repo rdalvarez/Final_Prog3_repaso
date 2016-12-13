672a953ea66fa9b05b675461ad1bcca2bfabf6d6
rdalvarez <david_de_odin@hotmail.com>
2016-12-13 13:08:41 -0300

    no le gusta los objetos :(

--- clases/materiales.php
+++ clases/materiales.php
@@ -67,17 +67,17 @@ class MaterialesTXT
 		while (!feof($a)) {
 			$arr = explode(" - ", fgets($a));
 			if (count($arr) > 1) {
-				// $material = array();
-				// $material['id'] = $arr[0];
-				// $material['nombre'] = $arr[1];
-				// $material['precio'] = $arr[2];
-				// $material['tipo'] = trim($arr[3]);
+				$material = array();
+				$material['id'] = $arr[0];
+				$material['nombre'] = $arr[1];
+				$material['precio'] = $arr[2];
+				$material['tipo'] = trim($arr[3]);
 
-				$material = new MaterialesTXT();
-				$material->id = $arr[0];
-				$material->nombre = $arr[1];
-				$material->precio = $arr[2];
-				$material->tipo = $arr[3];
+				// $material = new MaterialesTXT();
+				// $material->id = $arr[0];
+				// $material->nombre = $arr[1];
+				// $material->precio = $arr[2];
+				// $material->tipo = $arr[3];
 
 				array_push($arrMateriales, $material);
 			}
